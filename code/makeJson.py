37e9b1e49d1cac60c23fc201201646546b487b68
Sean Segal <Sean_Segal@brown.edu>
2017-05-06 16:58:17 -0400

    Rename final data files

--- code/makeJson.py
+++ code/makeJson.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 2:
 
 st = sys.argv[1]
 
-outputFile = "../data/complete/%s.json" % st
+outputFile = "../data/complete/data-%s.json" % st
 linksFile = "../data/%s/weights-%s.csv" % (st, st)
 senatorsFile = "../data/%s/senator-info-%s.json" % (st, st)
 
