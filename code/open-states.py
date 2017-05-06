e6b69042c6a36a7d7830cabd2587b84bec54db58
Sean Segal <Sean_Segal@brown.edu>
2017-05-06 14:11:34 -0400

    Montanta votes updated

--- code/open-states.py
+++ code/open-states.py
@@ -3,18 +3,19 @@ import csv
 import pprint
 import json
 
+# Useful for debugging
 pp = pprint.PrettyPrinter(indent=4)
 
-# fb = pyopenstates.get_bill(uid='NEB00005643');
-# pp.pprint(fb)
 
 st = 'mt'
-OUTFILE = '../data/montana-votes.csv'
+OUTFILE_VOTES = '../data/votes-%s.csv' % st
+OUTFILE_SENATOR_INFO = '../data/senator-info-raw-%s.json' % st
 
-#
+# Get all general information of all bills
 bills = pyopenstates.search_bills(state=st, search_window='all', fields=['id', 'bill_id'])
-print('BILLS: ', len(bills))
+print('TOTAL BILLS: ', len(bills))
 
+# Fetches extra information on each bill (voting information)
 legislators = set()
 votes = []
 count = 0
@@ -42,19 +43,7 @@ for bill in bills:
             voteRecord[other['leg_id']] = 'NV'
         votes.append(voteRecord)
 
-
-# UNCOMMENT TO WRITE VOTES TO FILE
-# with open(OUTFILE, 'w') as csvfile:
-#     print('Writing Dataset...')
-#     headers = ['date', 'description']
-#     headers.extend(list(legislators))
-#     print(len(list(legislators)))
-#     writer = csv.DictWriter(csvfile, fieldnames=headers, restval='N/A')
-#     writer.writeheader()
-#     for vote in votes:
-#         writer.writerow(vote)
-
-# UNCOMMENT TO WRITE SENATOR INFO TO FILE
+# Fetches information on each senator (name, party, district ..etc)
 senators = []
 for leg in legislators:
     try:
@@ -89,6 +78,21 @@ for leg in legislators:
     except Exception as e:
         print('ERROR: ' + str(e))
 
-filename = '../data/senator-info-raw-%s.json' % st
-with open(filename, 'w') as outfile:
+
+# WRITE INFORMATION TO FILE #
+
+# Write vote information to CSV file
+with open(OUTFILE_VOTES, 'w') as csvfile:
+    print('Writing votes to %s' % OUTFILE_VOTES)
+    headers = ['date', 'description']
+    headers.extend(list(legislators))
+    print(len(list(legislators)))
+    writer = csv.DictWriter(csvfile, fieldnames=headers, restval='N/A')
+    writer.writeheader()
+    for vote in votes:
+        writer.writerow(vote)
+
+# Write senator information to JSON file
+with open(OUTFILE_SENATOR_INFO, 'w') as outfile:
+    print('Writing senator information to %s' % OUTFILE_SENATOR_INFO)
     json.dump(senators, outfile)
