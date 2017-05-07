0b4ecb7adf9dd94c5b8a614a9eaa2219beb13a75
nathanlmeyers <nathanlmeyers@gmail.com>
2017-05-07 11:44:14 -0400

    updated summary code

--- code/summary-state.R
+++ code/summary-state.R
@@ -5,8 +5,8 @@ args <- commandArgs(trailingOnly = TRUE)
 state <- args[1] 
 folder <- args[2]
 
-state <- "al"
-folder <- "/Users/nathanmeyers/Documents/parity-in-the-senate/code"
+#state <- "mt"
+#folder <- "/Users/nathanmeyers/Documents/parity-in-the-senate/code"
 
 setwd(folder)
 
@@ -18,8 +18,6 @@ if (state == "ri"){
 }else{
   offset = 2
 }
-
-grepl("parities.", )
 
 years = sort(substr(colnames(info[,c(3:(length(info)-offset))]),10,13))
 if (length(years) == 0){
@@ -38,4 +36,44 @@ for (k in years){
   out[k,4] <- length(infoyear) - out[k,1] - out[k,2] - out[k,3]
 }
 
+#Parity histogram
+parity <- read.csv(paste0("../data/",state,"/parity-",state,".csv"))
+
+years <- substr(colnames(parity[,c(2:length(parity))]),13,16)
+colnames(parity) <- c("id",years)
+years <- sort(years)
+
+breaks = c(0:20)*0.05
+bin_names <- paste0("pbin",c(1:(length(breaks)-1)))
+histbins <- matrix(0,nrow = length(years),ncol = (length(breaks)-1))
+colnames(histbins) <- bin_names
+
+for (i in c(1:length(years))){
+  yr <- years[i]
+  x <- hist(parity[,yr],breaks = breaks)
+  histbins[i,] <- x$counts
+}
+
+out <- cbind(out,histbins)
+
+#Weight histogram
+weights <- read.csv(paste0("../data/",state,"/weights-",state,".csv"))
+years <- substr(colnames(weights[,c(3:length(weights))]),7,11)
+colnames(weights) <- c("Sen1","Sen2",years)
+years <- sort(years)
+
+breaks = c(0:100)*0.01
+bin_names <- paste0("wbin",c(1:(length(breaks)-1)))
+histbins <- matrix(0,nrow = length(years),ncol = (length(breaks)-1))
+colnames(histbins) <- bin_names
+
+for (i in c(1:length(years))){
+  yr <- years[i]
+  x <- hist(weights[,yr],breaks = breaks)
+  histbins[i,] <- x$counts
+}
+
+out <- cbind(out,histbins)
+
 write.csv(out,paste0("../data/",state,"/summary-",state,".csv"))
+
